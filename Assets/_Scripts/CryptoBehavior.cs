5a1fdb8f65a790344e5ca366ce897d276ab7ee53
Chadwick Lapis <c_lapis4@hotmail.com>
2021-02-22 18:54:39 -0500

    LOS working w/ triggers

--- Assets/_Scripts/CryptoBehavior.cs
+++ Assets/_Scripts/CryptoBehavior.cs
@@ -13,9 +13,13 @@ public enum CryptoState
 public class CryptoBehavior : MonoBehaviour
 {
     [Header("Line of Sight")]
+    /*
     public LayerMask collissionLayer;
     public Vector3 LOSoffset = new Vector3(0f, 2f, -5f);
+    */
     public bool hasLOS;
+
+    private GameObject player;
 
     private NavMeshAgent agent;
     private Animator animator;
@@ -29,9 +33,34 @@ public class CryptoBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
+        RaycastHit hit;
         var size = new Vector3(4f, 2f, 10f);
         //hasLOS = Physics.BoxCast(transform.position + LOSoffset, transform.localScale, transform.forward, transform.rotation, 10f, collissionLayer);
-        hasLOS = Physics.BoxCast(transform.position + LOSoffset, size * 0.5f, transform.forward, transform.rotation, 20f, collissionLayer);
+        hasLOS = Physics.BoxCast(transform.position + LOSoffset, size, transform.forward, out hit, transform.rotation, 0f, collissionLayer);
+        //hasLOS = Physics.BoxCast(transform.position + LOSoffset, size * 0.5f, transform.forward, transform.rotation, 10f);
+        */
+
+        if (hasLOS)
+        {
+            agent.SetDestination(player.transform.position);
+
+            if (Vector3.Distance(transform.position, player.transform.position) < 2.5)
+            {
+                animator.SetInteger("AnimState", (int)CryptoState.IDLE);
+                transform.LookAt(transform.position - player.transform.forward);
+            }
+            else
+            {
+                animator.SetInteger("AnimState", (int)CryptoState.RUN);
+            }
+        }
+        else
+        {
+            animator.SetInteger("AnimState", (int)CryptoState.IDLE);
+        }
+
+        /*
         if (Input.GetKeyDown(KeyCode.I))
         {
             animator.SetInteger("AnimState", (int)CryptoState.IDLE);
@@ -44,11 +73,31 @@ public class CryptoBehavior : MonoBehaviour
         {
             animator.SetInteger("AnimState", (int)CryptoState.JUMP);
         }
+        */
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log(other.gameObject.name);
+        if (other.gameObject.CompareTag("Player"))
+        {
+            hasLOS = true;
+            player = other.gameObject;
+        }
     }
 
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            hasLOS = false;
+        }
+    }
+
+    /*
     void OnDrawGizmos()
     {
         Gizmos.color = Color.magenta;
-        Gizmos.DrawWireCube(transform.position + new Vector3(0f,2f,-5f), new Vector3(4f, 2f, 10f));
+        Gizmos.DrawWireCube(transform.position + LOSoffset + transform.forward, new Vector3(4f, 2f, 10f));
     }
+    */
 }
